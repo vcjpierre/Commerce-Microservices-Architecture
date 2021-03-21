772d51d16b4687c86567a90bfaf8358cdacf66e4
Jean Pierre Velez <vcjpierre@gmail.com>
2021-03-21 17:25:11 -0400

    Init catalog persistence database project

---
+++ src/Services/Catalog/Catalog.Persistence.Database/ApplicationDbContext.cs
@@ -0,0 +1,16 @@
+﻿using Catalog.Domain;
+using Microsoft.EntityFrameworkCore;
+
+namespace Catalog.Persistence.Database
+{
+    public class ApplicationDbContext : DbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options)
+        {
+
+        }
+
+        public DbSet<Product> Products { get; set; }
+        public DbSet<ProductInStock> Stocks { get; set; }
+    }
+}
