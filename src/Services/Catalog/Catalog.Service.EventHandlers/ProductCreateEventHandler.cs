383518a221f91138a7ab1f8bdae2b625474a8c92
Jean Pierre Velez <vcjpierre@gmail.com>
2021-03-22 20:40:02 -0400

    Add catalog service event handlers

---
+++ src/Services/Catalog/Catalog.Service.EventHandlers/ProductCreateEventHandler.cs
@@ -0,0 +1,33 @@
+﻿using Catalog.Domain;
+using Catalog.Persistence.Database;
+using Catalog.Service.EventHandlers.Commands;
+using MediatR;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace Catalog.Service.EventHandlers
+{
+    public class ProductCreateEventHandler :
+        INotificationHandler<ProductCreateCommand>
+    {
+        private readonly ApplicationDbContext _context;
+
+        public ProductCreateEventHandler(
+            ApplicationDbContext context)
+        {
+            _context = context;
+        }
+
+        public async Task Handle(ProductCreateCommand notification, CancellationToken cancellationToken)
+        {
+            await _context.AddAsync(new Product
+            {
+                Name = notification.Name,
+                Description = notification.Description,
+                Price = notification.Price
+            });
+
+            await _context.SaveChangesAsync();
+        }
+    }
+}
---
+++ src/Services/Catalog/Catalog.Service.EventHandlers/ProductInStockUpdateStockEventHandler.cs
@@ -0,0 +1,77 @@
+﻿using Catalog.Domain;
+using Catalog.Persistence.Database;
+using Catalog.Service.EventHandlers.Commands;
+using Catalog.Service.EventHandlers.Exceptions;
+using MediatR;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Linq;
+using System.Threading;
+using System.Threading.Tasks;
+using static Catalog.Common.Enums;
+
+namespace Catalog.Service.EventHandlers
+{
+    public class ProductInStockUpdateStockEventHandler :
+        INotificationHandler<ProductInStockUpdateStockCommand>
+    {
+        private readonly ApplicationDbContext _context;
+        private readonly ILogger<ProductInStockUpdateStockEventHandler> _logger;
+
+        public ProductInStockUpdateStockEventHandler(
+            ApplicationDbContext context,
+            ILogger<ProductInStockUpdateStockEventHandler> logger)
+        {
+            _context = context;
+            _logger = logger;
+        }
+
+        public async Task Handle(ProductInStockUpdateStockCommand notification, CancellationToken cancellationToken)
+        {
+            _logger.LogInformation("--- ProductInStockUpdateStockCommand started");
+
+            var products = notification.Items.Select(x => x.ProductId);
+            var stocks = await _context.Stocks.Where(x => products.Contains(x.ProductId)).ToListAsync();
+
+            _logger.LogInformation("--- Retrieve products from database");
+
+            foreach (var item in notification.Items)
+            {
+                var entry = stocks.SingleOrDefault(x => x.ProductId == item.ProductId);
+
+                if (item.Action == ProductInStockAction.Substract)
+                {
+                    if (entry == null || item.Stock > entry.Stock)
+                    {
+                        _logger.LogError($"--- Product {entry.ProductId} -doens't have enough stock");
+                        throw new ProductInStockUpdateStockCommandException($"Product {entry.ProductId} - doens't have enough stock");
+                    }
+
+                    entry.Stock -= item.Stock;
+
+                    _logger.LogInformation($"--- Product {entry.ProductId} - its stock was subtracted and its new stock is {entry.Stock}");
+                }
+                else
+                {
+                    if (entry == null)
+                    {
+                        entry = new ProductInStock
+                        {
+                            ProductId = item.ProductId
+                        };
+
+                        _logger.LogInformation($"--- New stock record was created for {entry.ProductId} because didn't exists before");
+
+                        await _context.AddAsync(entry);
+                    }
+
+                    _logger.LogInformation($"--- Add stock to product {entry.ProductId}");
+                    entry.Stock += item.Stock;
+                }
+            }
+
+            await _context.SaveChangesAsync();
+        }
+    }
+}
