a18d31c5238a9e81c00d19ec3b40af05485d12b0
Jean Pierre Velez <vcjpierre@gmail.com>
2021-03-22 20:37:56 -0400

    Add product controller

---
+++ src/Services/Catalog/Catalog.Api/Controllers/ProductController.cs
@@ -0,0 +1,60 @@
+﻿
+using Catalog.Service.EventHandlers.Commands;
+using Catalog.Service.Queries;
+using Catalog.Service.Queries.DTOs;
+using MediatR;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using Service.Common.Collection;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Catalog.Api.Controllers
+{
+    [ApiController]
+    [Route("v1/products")]
+    public class ProductController : ControllerBase
+    {
+        private readonly IProductQueryService _productQueryService;
+        private readonly ILogger<ProductController> _logger;
+        private readonly IMediator _mediator;
+
+        public ProductController(ILogger<ProductController> logger, IProductQueryService productQueryService, IMediator mediator)
+        {
+            _logger = logger;
+            _productQueryService = productQueryService;
+            _mediator = mediator;
+
+        }
+
+        [HttpGet]
+        public async Task<DataCollection<ProductDto>> GetAll(int page = 1, int take = 10, string ids = null)
+        {
+            IEnumerable<int> products = null;
+
+            if (!string.IsNullOrEmpty(ids))
+            {
+                products = ids.Split(',').Select(x => Convert.ToInt32(x));
+            }
+
+            return await _productQueryService.GetAllAsync(page, take, products);
+
+        }
+
+        [HttpGet("{id}")]
+        public async Task<ProductDto> Get(int id)
+        {
+            return await _productQueryService.GetAsync(id);
+        }
+
+
+        [HttpPost]
+        public async Task<IActionResult> Create(ProductCreateCommand notification)
+        {
+            await _mediator.Publish(notification);
+            return Ok();
+        }
+    }
+}
