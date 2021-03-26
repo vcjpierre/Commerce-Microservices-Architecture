474de474175543995e0909e1e48dfafad3217fae
Jean Pierre Velez <vcjpierre@gmail.com>
2021-03-26 18:15:39 -0400

    Add api gateway proxies

---
+++ src/Gateways/Api.Gateway.Proxies/ApiUrls.cs
@@ -0,0 +1,10 @@
+﻿namespace Api.Gateway.Proxy
+{
+    public class ApiUrls
+    {
+        public string IdentityUrl { get; set; }
+        public string CatalogUrl { get; set; }
+        public string CustomerUrl { get; set; }
+        public string OrderUrl { get; set; }
+    }
+}
---
+++ src/Gateways/Api.Gateway.Proxies/OrderProxy.cs
@@ -0,0 +1,78 @@
+﻿using Api.Gateway.Models;
+using Api.Gateway.Models.Order.DTOs;
+using Api.Gateway.Models.Orders.Commands;
+using Api.Gateway.Proxies.Config;
+using Api.Gateway.Proxy;
+using Microsoft.AspNetCore.Http;
+using Microsoft.Extensions.Options;
+using System.Net.Http;
+using System.Text;
+using System.Text.Json;
+using System.Threading.Tasks;
+
+namespace Api.Gateway.Proxies
+{
+    public interface IOrderProxy
+    {
+        Task<DataCollection<OrderDto>> GetAllAsync(int page, int take);
+        Task<OrderDto> GetAsync(int id);
+        Task CreateAsync(OrderCreateCommand command);
+    }
+
+    public class OrderProxy : IOrderProxy
+    {
+        private readonly ApiUrls _apiUrls;
+        private readonly HttpClient _httpClient;
+
+        public OrderProxy(
+            HttpClient httpClient,
+            IOptions<ApiUrls> apiUrls,
+            IHttpContextAccessor httpContextAccessor)
+        {
+            httpClient.AddBearerToken(httpContextAccessor);
+
+            _httpClient = httpClient;
+            _apiUrls = apiUrls.Value;
+        }
+
+        public async Task<DataCollection<OrderDto>> GetAllAsync(int page, int take)
+        {
+            var request = await _httpClient.GetAsync($"{_apiUrls.OrderUrl}v1/orders?page={page}&take={take}");
+            request.EnsureSuccessStatusCode();
+
+            return JsonSerializer.Deserialize<DataCollection<OrderDto>>(
+                await request.Content.ReadAsStringAsync(),
+                new JsonSerializerOptions
+                {
+                    PropertyNameCaseInsensitive = true
+                }
+            );
+        }
+
+        public async Task<OrderDto> GetAsync(int id)
+        {
+            var request = await _httpClient.GetAsync($"{_apiUrls.OrderUrl}v1/orders/{id}");
+            request.EnsureSuccessStatusCode();
+
+            return JsonSerializer.Deserialize<OrderDto>(
+                await request.Content.ReadAsStringAsync(),
+                new JsonSerializerOptions
+                {
+                    PropertyNameCaseInsensitive = true
+                }
+            );
+        }
+
+        public async Task CreateAsync(OrderCreateCommand command)
+        {
+            var content = new StringContent(
+                JsonSerializer.Serialize(command),
+                Encoding.UTF8,
+                "application/json"
+            );
+
+            var request = await _httpClient.PostAsync($"{_apiUrls.OrderUrl}v1/orders", content);
+            request.EnsureSuccessStatusCode();
+        }
+    }
+}
