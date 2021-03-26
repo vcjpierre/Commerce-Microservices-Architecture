0fdb447ac6ab50ffe796d0774cfc625106c741c3
Jean Pierre Velez <vcjpierre@gmail.com>
2021-03-26 18:15:22 -0400

    Add api gateway models

---
+++ src/Gateways/Api.Gateway.Models/Order/DTOs/OrderDto.cs
@@ -0,0 +1,31 @@
+﻿using Api.Gateway.Models.Catalog.DTOs;
+using Api.Gateway.Models.Customer.DTOs;
+using System;
+using System.Collections.Generic;
+using static Api.Gateway.Models.Order.Commons.Enums;
+
+namespace Api.Gateway.Models.Order.DTOs
+{
+    public class OrderDto
+    {
+        public int OrderId { get; set; }
+        public ClientDto Client { get; set; }
+        public string OrderNumber { get; set; }
+        public OrderStatus Status { get; set; }
+        public OrderPayment PaymentType { get; set; }
+        public int ClientId { get; set; }
+        public IEnumerable<OrderDetailDto> Items { get; set; } = new List<OrderDetailDto>();
+        public DateTime CreatedAt { get; set; }
+        public decimal Total { get; set; }
+    }
+
+    public class OrderDetailDto
+    {
+        public ProductDto Product { get; set; }
+        public int OrderDetailId { get; set; }
+        public int ProductId { get; set; }
+        public int Quantity { get; set; }
+        public decimal UnitPrice { get; set; }
+        public decimal Total { get; set; }
+    }
+}
