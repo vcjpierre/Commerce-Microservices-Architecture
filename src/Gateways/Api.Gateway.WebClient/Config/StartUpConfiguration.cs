d9f82cef6ce7cdd230b6b4b7a45b0342199190ce
Jean Pierre Velez <vcjpierre@gmail.com>
2021-03-26 18:16:55 -0400

    Add webclient

---
+++ src/Gateways/Api.Gateway.WebClient/Config/StartUpConfiguration.cs
@@ -0,0 +1,33 @@
+﻿using Api.Gateway.Proxies;
+using Api.Gateway.Proxy;
+using Microsoft.AspNetCore.Http;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Diagnostics.HealthChecks;
+using System.IO;
+using System.Text;
+using System.Text.Json;
+using System.Threading.Tasks;
+
+namespace Api.Gateway.WebClient.Config
+{
+    public static class StartUpConfiguration
+    {
+        public static IServiceCollection AddAppsettingBinding(this IServiceCollection service, IConfiguration configuration)
+        {
+            service.Configure<ApiUrls>(opts => configuration.GetSection("ApiUrls").Bind(opts));
+            return service;
+        }
+
+        public static IServiceCollection AddProxiesRegistration(this IServiceCollection service, IConfiguration configuration)
+        {
+            service.AddHttpContextAccessor();
+
+            service.AddHttpClient<IOrderProxy, OrderProxy>();
+            service.AddHttpClient<ICustomerProxy, CustomerProxy>();
+            service.AddHttpClient<ICatalogProxy, CatalogProxy>();
+
+            return service;
+        }
+    }
+}
---
+++ src/Gateways/Api.Gateway.WebClient/Startup.cs
@@ -0,0 +1,75 @@
+using Api.Gateway.WebClient.Config;
+using Common.Logging;
+using Microsoft.AspNetCore.Authentication.JwtBearer;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using Microsoft.Extensions.Logging;
+using Microsoft.IdentityModel.Tokens;
+using System.Text;
+
+namespace Api.Gateway.WebClient
+{
+    public class Startup
+    {
+        public Startup(IConfiguration configuration)
+        {
+            Configuration = configuration;
+        }
+
+        public IConfiguration Configuration { get; }
+
+        // This method gets called by the runtime. Use this method to add services to the container.
+        public void ConfigureServices(IServiceCollection services)
+        {
+            services.AddAppsettingBinding(Configuration)
+                    .AddProxiesRegistration(Configuration);
+
+            services.AddControllers();
+
+            var secretKey = Encoding.ASCII.GetBytes(
+                Configuration.GetValue<string>("SecretKey")
+            );
+
+            services.AddAuthentication(JwtBearerDefaults.AuthenticationScheme).AddJwtBearer(x =>
+            {
+                x.RequireHttpsMetadata = false;
+                x.SaveToken = true;
+                x.TokenValidationParameters = new TokenValidationParameters
+                {
+                    ValidateIssuerSigningKey = true,
+                    IssuerSigningKey = new SymmetricSecurityKey(secretKey),
+                    ValidateIssuer = false,
+                    ValidateAudience = false
+                };
+            });
+        }
+
+        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+        public void Configure(IApplicationBuilder app, IWebHostEnvironment env, ILoggerFactory loggerFactory)
+        {
+            if (env.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+            }
+            else
+            {
+                loggerFactory.AddSyslog(
+                    Configuration.GetValue<string>("Papertrail:host"),
+                    Configuration.GetValue<int>("Papertrail:port"));
+            }
+
+            app.UseRouting();
+
+            app.UseAuthorization();
+            app.UseAuthentication();
+
+            app.UseEndpoints(endpoints =>
+            {
+                endpoints.MapControllers();
+            });
+        }
+    }
+}
